33d8d3e81ea9e2a2363c024e576648f7bc354ebc
Bela Vlahovics <vlahobgm@gmail.com>
2020-03-25 16:29:20 +0100

    todo

--- src/Todo/FileHandler.java
+++ src/Todo/FileHandler.java
@@ -1,5 +1,6 @@
 package Todo;
 
+import com.sun.tools.javac.Main;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +18,15 @@ public class FileHandler {
       content = Files.readAllLines(filepath);
     } catch (IOException e) {
       System.out.println("Cant read the file...");;
+    }
+  }
+
+  public void writeFile(){
+    readFile();
+    try {
+      Files.write(filepath,newContent);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
--- src/Todo/Todo.java
+++ src/Todo/Todo.java
@@ -1,19 +1,17 @@
 package Todo;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sound.sampled.Control;
 
 public class Todo {
   public static void main(String[] args) {
-    /*int counter =1;
-    for (String arg: args) {
-      System.out.println(counter + " : " + arg);
-      counter++;
-    }*/
+
     if (args.length == 0){
       PrintUsage printUsage = new PrintUsage();
       printUsage.PrintUsage();
@@ -23,6 +21,11 @@ public class Todo {
     if (args.length > 0 && args[0].equals("-l")) {
       ListTasks listTasks = new ListTasks();
       listTasks.ListTasks();
+
+    }
+
+    if (args.length == 2 && args[0].equals("-a")){
+      FileHandler fileHandler = new FileHandler();
 
     }
 
